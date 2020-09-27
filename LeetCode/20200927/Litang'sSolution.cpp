828195b7c6a571890539a6325547230de014f5e5
ak <64268365+Bug-terminator@users.noreply.github.com>
2020-09-27 09:49:32 +0800

    Update and rename Litang'sSolution to Litang'sSolution.cpp

--- LeetCode/20200927/Litang'sSolution.cpp
+++ LeetCode/20200927/Litang'sSolution.cpp
@@ -1,3 +1,10 @@
+/*
+   Author: litang
+   Description: 235. 二叉搜索树的最近公共祖先
+   Since: 2020/09/27
+          09:05:38
+*/
+
 class Solution {
     //并查集(此解答不仅适用于BST)
     unordered_map<TreeNode *, TreeNode*> father;
@@ -25,7 +32,9 @@ public:
     }
 };
 //待改进:p,q的父节点都找到时停止递归(减枝),用vector<int>并查集,用两个map实现TreeNode*到int的转换
-//改进:两个节点同时遍历(leetcode官方题解,仅适用于BST)
+
+
+//题解2:两个节点同时遍历(leetcode官方题解,仅适用于BST)
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
